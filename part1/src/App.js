166c2713e5ed3319a780dc3afd12a57e8e596608
Myat Ko Ko <myatkk@ccn.com.sgm>
2022-08-08 19:00:37 +0800

    part1 initial commit

---
+++ part1/src/App.js
@@ -0,0 +1,60 @@
+import ReactDOM from "react-dom"
+//import App from "./App"
+
+/*
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Footer />
+    </>
+  )
+}
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href="https://github.com/EdricMyatKoKo/fullstackopen2022">Edric Myat Ko Ko</a>
+    </div>
+  )
+}
+
+export default App;
